fce4d891d6070e804d5a9162898d7fc4ba1d6a29
Emily Fernschild <emilyfernschild@gmail.com>
2023-01-23 12:42:42 -0500

    added skills

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,10 +1,45 @@
+import react from './images/react.png';
+import Ruby from './images/Ruby.png';
+import rails from './images/rails.png';
+import github from './images/github.png';
+import vscode from './images/Visual_Studio_Code.png';
+import postgresql from './images/Postgresql.png';
+import javascript from './images/javascript.png';
+import html from './images/HTML.png';
+import css from './images/CSS.png';
+import postman from './images/postman.png';
+import Bootstrap from './images/Bootstrap.png';
+import npm from './images/npm.png';
 
 
 function Skills(){
     return(
         <div id='skills'>
             <h2>Skills </h2>
-            {/* <img src='https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='pic'></img> */}
+            <img src={react} alt='pic'></img>
+            <h4> React</h4>
+            <img src={Ruby} alt='pic'></img>
+            <h4> Ruby</h4>
+            <img src={rails} alt='pic'></img>
+            <h4> Ruby on Rails</h4>
+            <img src={github} alt='pic'></img>
+            <h4> GitHub</h4>
+            <img src={vscode} alt='pic'></img>
+            <h4> VSCode</h4>
+            <img src={postgresql} alt='pic'></img>
+            <h4> Postgresql</h4>
+            <img src={javascript} alt='pic'></img>
+            <h4> JavaScript</h4>
+            <img src={html} alt='pic'></img>
+            <h4> HTML</h4>
+            <img src={css} alt='pic'></img>
+            <h4> CSS</h4>
+            <img src={postman} alt='pic'></img>
+            <h4> Postman</h4>
+            <img src={Bootstrap} alt='pic'></img>
+            <h4> Boostrap</h4>
+            <img src={npm} alt='pic'></img>
+            <h4> npm</h4>
         </div>
     )
 }
