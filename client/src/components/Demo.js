dbc6c8fb6061bbde13703434fbee886425d50522
Emily Fernschild <emilyfernschild@gmail.com>
2023-01-31 15:38:04 -0500

    added carousel

--- client/src/components/Demo.js
+++ client/src/components/Demo.js
@@ -1,4 +1,5 @@
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Demo(){
   return (
@@ -8,8 +9,21 @@ function Demo(){
         <h3 className="more-demo">More to come!</h3>
       </div>
       <div className="carousel">
-        <h2>insert videos here</h2>
-      </div>
+        <Carousel infiniteLoop dynamicHeight >
+                  <div>
+                      <img src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+                      {/* <p className="legend">Legend 1</p> */}
+                  </div>
+                  <div>
+                      <img src="https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                      {/* <p className="legend">Legend 2</p> */}
+                  </div>
+                  <div>
+                      <img src="https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                      {/* <p className="legend">Legend 3</p> */}
+                  </div>
+              </Carousel>
+        </div>
     </div>
   ) 
 }
