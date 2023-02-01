d0b8a8bd31e63ccb5c2c9eb138d796247ba37fe5
Emily Fernschild <emilyfernschild@gmail.com>
2023-02-01 15:36:10 -0500

    some header fixes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import logo from './images/ef_logo.png';
 
 function Header(){
+    const [fix, setFix] = useState(false);
+
+    function setFixed(){
+        if (window.scrollY >= 10){
+            setFix(true)
+        }else {
+            setFix(false)
+        }
+    }
+    window.addEventListener('scroll',setFixed)
     return(
         <header className='header'>
-            <nav className='navbar'>
+            <nav className={fix ? 'navbar' : 'navbar-fix'}>
                 <a href='/'>
                     <img className='logo' src={logo} alt='logo-img'/>
                 </a>
