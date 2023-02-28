2e904daf3cf516b9b68109924a2cac394c545522
Emily Fernschild <emilyfernschild@gmail.com>
2023-02-28 15:10:03 -0500

    added css to iframe

--- client/src/components/Demo.js
+++ client/src/components/Demo.js
@@ -8,18 +8,18 @@ function Demo(){
         <h2 className="demo-intro">Demos.</h2>
         <h3 className="more-demo">More to come!</h3>
       </div>
-      <div className="carousel">
-        <Carousel >
-                  <div>
-                      <iframe width="500" height="450" className="videos" title="puppy-prowl" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
-                  </div>
-                  <div>
-                      <iframe width="500px" height="450px" className="videos" title="puppy-prowl2" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
-                  </div>
-                  <div>
-                      <iframe width="500px" height="450px" className="videos" title="puppy-prowl3" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
-                  </div>
-              </Carousel>
+      <div className="carousel" >
+        <Carousel showThumbs={false} >
+          <div>
+            <iframe width="450" height="500" title="puppy-prowl" id="inlineFrameExample" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
+          </div>
+          <div>
+            <iframe width="450" height="500" className="videos" id="inlineFrameExample" title="puppy-prowl2" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
+          </div>  
+          <div>
+            <iframe width="450" height="500" className="videos" id="inlineFrameExample" title="puppy-prowl3" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
+          </div>
+        </Carousel>
         </div>
     </div>
   ) 
