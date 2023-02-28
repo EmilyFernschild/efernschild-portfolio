9b654fe66a699bfd6e9c22d914ac3f72cfb9ba80
Emily Fernschild <emilyfernschild@gmail.com>
2023-02-28 14:31:48 -0500

    added one demo

--- README.md
+++
@@ -1,3 +0,0 @@
-# README
-
-This project is my personal portfolio to showcase projects I have done through my education at Flatiron School. Along with information about myself, my skills, and links to my social networks.
--- client/src/components/Demo.js
+++ client/src/components/Demo.js
@@ -9,18 +9,15 @@ function Demo(){
         <h3 className="more-demo">More to come!</h3>
       </div>
       <div className="carousel">
-        <Carousel infiniteLoop dynamicHeight >
+        <Carousel >
                   <div>
-                      <img src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
-                      {/* <p className="legend">Legend 1</p> */}
+                      <iframe width="500" height="450" className="videos" title="puppy-prowl" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
                   </div>
                   <div>
-                      <img src="https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-                      {/* <p className="legend">Legend 2</p> */}
+                      <iframe width="500px" height="450px" className="videos" title="puppy-prowl2" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
                   </div>
                   <div>
-                      <img src="https://images.unsplash.com/photo-1477884213360-7e9d7dcc1e48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-                      {/* <p className="legend">Legend 3</p> */}
+                      <iframe width="500px" height="450px" className="videos" title="puppy-prowl3" src="https://www.youtube.com/embed/9LWAuwXDsr0"></iframe>
                   </div>
               </Carousel>
         </div>
