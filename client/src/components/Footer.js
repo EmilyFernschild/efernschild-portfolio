332bec0602e5bc929c161eb4eca5a034fcbd8832
Emily Fernschild <emilyfernschild@gmail.com>
2023-07-07 13:22:01 -0400

    tweaked footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,20 +11,23 @@ function Footer() {
         <h1 className='contact-me'> contact me</h1>
         <h4 className='email'> emilyfernschild@gmail.com</h4>
       </div>
-      <div className='contact-links'>
-        <a href='https://www.linkedin.com/in/emily-fernschild/'>
-          <img className='contact-logo' src={linkedin} alt='linkedIn'/>
-        </a>
-        <a href='https://github.com/EmilyFernschild'>
-          <img className='contact-logo' src={github} alt='github'/>
-        </a>
-        <a href='https://dev.to/emilyfernschild'>
-          <img className='contact-logo' id='dev' src={dev} alt='blog'/>
-        </a>
-        {/* replace when resume done */}
-        <a href='https://docs.google.com/document/d/1rXTmuq1IzS_YdQo68_BuS34OluqSgxjgdzu0Wvpomfw/edit'>
-          <img className='contact-logo' src={resume} alt='resume'/>
-        </a>
+      <div className='click-text'>
+        <h1 className='click-me'> click me</h1>
+        <div className='contact-links'>
+          <a href='https://www.linkedin.com/in/emily-fernschild/'>
+            <img className='contact-logo' src={linkedin} alt='linkedIn'/>
+          </a>
+          <a href='https://github.com/EmilyFernschild'>
+            <img className='contact-logo' src={github} alt='github'/>
+          </a>
+          <a href='https://dev.to/emilyfernschild'>
+            <img className='contact-logo' id='dev' src={dev} alt='blog'/>
+          </a>
+          {/* replace when resume done */}
+          <a href='https://docs.google.com/document/d/1rXTmuq1IzS_YdQo68_BuS34OluqSgxjgdzu0Wvpomfw/edit'>
+            <img className='contact-logo' src={resume} alt='resume'/>
+          </a>
+        </div>
       </div>
     </div>
   )
